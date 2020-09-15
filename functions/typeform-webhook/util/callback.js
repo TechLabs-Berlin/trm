a90251de39041800a621497ef128a77c59ed2b8a
Felix Seidel <felix@seidel.me>
2020-09-15 16:54:23 +0200

    :recycle: Rename function typeform-import-response -> typeform

--- functions/typeform-webhook/util/callback.js
+++
@@ -1,8 +0,0 @@
-const querystring = require('querystring')
-
-module.exports = {
-  createFullCallbackURL: ({callbackURL, formID}) => {
-    const query = querystring.stringify({ op: 'one', formID })
-    return `${callbackURL}?${query}`
-  }
-}
