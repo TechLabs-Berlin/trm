527d90a168d5b6d2da44223b6047d7f9e655d532
Maximilian Schall <Maxscha@users.noreply.github.com>
2022-03-01 15:22:13 +0100

    Update termSelect.js

--- frontend/src/inputs/termSelect.js
+++ frontend/src/inputs/termSelect.js
@@ -7,6 +7,9 @@ export const TermSelectInput = ({ source, record = {}, ...rest}) => {
     { id: '2020_01', name: '2020-01 (Summer)' },
     { id: '2020_02', name: '2020-02 (Winter)' },
     { id: '2021_01', name: '2021-01 (Summer)' },
+    { id: '2021_02', name: '2021-02 (Winter)' },
+    { id: '2022_01', name: '2022-01 (Summer)' },
+    { id: '2022_02', name: '2022-02 (Winter)' },
   ]} {...rest} />
 }
 
